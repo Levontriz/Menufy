ca836a11b2fde44ed9cfbdef7f296fe883b6acb6
Levon Booth <boothlevon@gmail.com>
2025-07-25 10:05:47 +1200

    Clean up + regex
    Cleaned up variables and functions names to comply with PyCharm's template and added a regex for the namespace of screens

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ manager: ScreenManager = ScreenManager()
 screen1: Screen = manager.add_screen("base:mainMenu")
 
 screen1.set_title("Main Menu") # Set the title of the screen. This does return the screen object for chaining.
-screen1.set_type(ScreenTypes.OPTIONS) # Set the type of the screen. This does return the screen object for chaining. This currently doesnt do anything, but it is here for future use.
+screen1.set_type(ScreenTypes.OPTIONS) # Set the type of the screen. This does return the screen object for chaining. This currently doesn't do anything, but it is here for future use.
 screen1.add_option("Start Game", lambda: print("Starting game...")) # Add an option to the screen. This does return the screen object for chaining. Instead of lambda you can also use a function.
 screen1.add_option("Settings", lambda: print("Opening settings...")) # Add another option to the screen.
 screen1.add_option("Exit", lambda: print("Exiting...")) # Add another option to the screen.
@@ -13,3 +13,7 @@ screen1.set_prompt("Select an option: ") # Set the prompt for the screen. This d
 screen1.set_prompt_cursor("> ") # Set the prompt cursor for the screen. This does return the screen object for chaining.
 # Display the screen
 screen1.display_screen()
+
+# Note: If you have the entire definition of the screen from display screen all the way to manager.add_screen inside a while loop it will inexplicably exit the loop after just one input.
+# To fix this you can define the screen outside the while loop and run screen.display_screen inside the loop
+# I still have no idea why it does this but it has an easy fix that I believe won't cause problems in any use case, if it is a problem contact me
